d36649e32d1b04352f387e81c538e80b6bd40021
Aingeru Sanchez <Aingeru1703@gmail.com>
2018-03-07 09:48:27 +0100

    ADD title a iconos UX Likes de UI  no editables style="user-select: none"

--- src/app/pipes/filter-recetas.pipe.ts
+++ src/app/pipes/filter-recetas.pipe.ts
@@ -5,15 +5,21 @@ import { Receta } from '../model/receta';
 export class FilterRecetas implements PipeTransform {
 
   transform(recetas: Receta[], searchText: string): Receta[] {
+    // Si no hay recetas, no devolver nada
     if (!recetas) { return []; }
+    // Si no hay texto para filtrar la búsqueda, return todas las recetas
     if (!searchText) { return recetas; }
     searchText = searchText.toLowerCase();
     let resultado = '';
+    // En caso de escribir termino de búsqueda 'searchText'
+    // buscar entre las recetas ese fragmento de texto
+    // en los atributos de nombre, cocinero e ingredientes
     return recetas.filter( recetaIter => {
-      resultado = recetaIter.nombre + '  ' + recetaIter.cocinero;
-      recetaIter.ingredientes.forEach( ingredIter => {
+      resultado = recetaIter.nombre + recetaIter.cocinero + recetaIter.ingredientes;
+      // CHANGES: sirve con añadir el array de ingredientes completo
+      /* recetaIter.ingredientes.forEach( ingredIter => {
         resultado += ' ' + ingredIter;
-      });
+      }); */
       resultado = resultado.toLowerCase();
       return resultado.includes(searchText);
     });
